b87417ce7a85fc5338da912675681037f5255b3b
Macbook <macbook@Macbooks-MacBook-Pro-3.local>
2018-01-07 17:29:52 +0700

    Embbed PageMenu

--- TaskListSwift/ViewController.swift
+++ TaskListSwift/ViewController.swift
@@ -7,12 +7,94 @@
 //
 
 import UIKit
+import PageMenu
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wrapperPageMenuView: UIView!
+    var pageMenu : CAPSPageMenu?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Array to keep track of controllers in page menu
+        var controllerArray : [UIViewController] = []
+        
+        // Create variables for all view controllers you want to put in the
+        // page menu, initialize them, and add each to the controller array.
+        // (Can be any UIViewController subclass)
+        // Make sure the title property of all view controllers is set
+        // Example:
+        let aVC = self.storyboard?.instantiateViewController(withIdentifier: "aVC")
+        aVC?.title = "A Screen"
+        controllerArray.append(aVC!)
+        
+        let bVC = self.storyboard?.instantiateViewController(withIdentifier: "bVC")
+        bVC?.title = "B Screen"
+        controllerArray.append(bVC!)
+        
+        let cVC = self.storyboard?.instantiateViewController(withIdentifier: "cVC")
+        cVC?.title = "C Screen"
+        controllerArray.append(cVC!)
+        
+        // Customize page menu to your liking (optional) or use default settings by sending nil for 'options' in the init
+        // Example:
+        let parameters: [CAPSPageMenuOption] = [
+            //options 1
+            //CAPSPageMenuOption.menuItemSeparatorWidth(4.3),
+            //CAPSPageMenuOption.useMenuLikeSegmentedControl(true),
+            //CAPSPageMenuOption.menuItemSeparatorPercentageHeight(0.1)
+            
+            /*option 2
+            .scrollMenuBackgroundColor(UIColor(red: 30.0/255.0, green: 30.0/255.0, blue: 30.0/255.0, alpha: 1.0)),
+            .viewBackgroundColor(UIColor(red: 20.0/255.0, green: 20.0/255.0, blue: 20.0/255.0, alpha: 1.0)),
+            .selectionIndicatorColor(UIColor.orange),
+            .bottomMenuHairlineColor(UIColor(red: 70.0/255.0, green: 70.0/255.0, blue: 80.0/255.0, alpha: 1.0)),
+            .menuItemFont(UIFont(name: "HelveticaNeue", size: 13.0)!),
+            .menuHeight(40.0),
+            .menuItemWidth(90.0),
+            .centerMenuItems(true)
+             */
+            
+            /*option 3
+            .scrollMenuBackgroundColor(UIColor(red: 20.0/255.0, green: 20.0/255.0, blue: 20.0/255.0, alpha: 1.0)),
+            .viewBackgroundColor(UIColor(red: 20.0/255.0, green: 20.0/255.0, blue: 20.0/255.0, alpha: 1.0)),
+            .selectionIndicatorColor(UIColor.orange),
+            .addBottomMenuHairline(false),
+            .menuItemFont(UIFont(name: "HelveticaNeue", size: 35.0)!),
+            .menuHeight(50.0),
+            .selectionIndicatorHeight(0.0),
+            .menuItemWidthBasedOnTitleTextWidth(true),
+            .selectedMenuItemLabelColor(UIColor.orange)
+            */
+            
+            /*option 4*/
+            .menuItemSeparatorWidth(4.3),
+            .scrollMenuBackgroundColor(UIColor.white),
+            .viewBackgroundColor(UIColor(red: 247.0/255.0, green: 247.0/255.0, blue: 247.0/255.0, alpha: 1.0)),
+            .bottomMenuHairlineColor(UIColor(red: 20.0/255.0, green: 20.0/255.0, blue: 20.0/255.0, alpha: 0.1)),
+            .selectionIndicatorColor(UIColor(red: 18.0/255.0, green: 150.0/255.0, blue: 225.0/255.0, alpha: 1.0)),
+            .menuMargin(20.0),
+            .menuHeight(40.0),
+            .selectedMenuItemLabelColor(UIColor(red: 18.0/255.0, green: 150.0/255.0, blue: 225.0/255.0, alpha: 1.0)),
+            .unselectedMenuItemLabelColor(UIColor(red: 40.0/255.0, green: 40.0/255.0, blue: 40.0/255.0, alpha: 1.0)),
+            .menuItemFont(UIFont(name: "HelveticaNeue-Medium", size: 14.0)!),
+            .useMenuLikeSegmentedControl(true),
+            .menuItemSeparatorRoundEdges(true),
+            .selectionIndicatorHeight(2.0),
+            .menuItemSeparatorPercentageHeight(0.1)
+        ]
+        
+        // Initialize page menu with controller array, frame, and optional parameters
+        pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: //CGRectMake(0.0,0.0,self.view.frame.width, self.view.frame.height)
+            CGRect(origin: .zero, size: CGSize(width: wrapperPageMenuView.frame.width, height: wrapperPageMenuView.frame.height))
+            , pageMenuOptions: parameters)
+        
+        // Lastly add page menu as subview of base view controller view
+        // or use pageMenu controller in you view hierachy as desired
+        wrapperPageMenuView.addSubview(pageMenu!.view)
+        pageMenu?.didMove(toParentViewController: self)
     }
 
     override func didReceiveMemoryWarning() {
